97daa1f5150128105631f8fbfe050d4a80720a13
luissantosjr <luissantosjr@outlook.com>
2018-10-03 16:10:27 -0500

    Finished

--- web.rb
+++ web.rb
@@ -4,13 +4,20 @@ require 'sinatra'
 #ex: localhost:4567/multiply?x=4&y=8
 #should display: 32
 get '/multiply' do
-	
+	numb1 = params["x"].to_i
+	numb2 = params["y"].to_i
+	result = numb1 * numb2
+	return result.to_s
 end
 
 #multiply
 #ex: localhost:4567/multiply?x=3.5&y=2.3
 #should display: 8.05
 get '/float_multiply' do
+	numb1 = params["x"].to_f
+	numb2 = params["y"].to_f
+	result = numb1 * numb2
+	return result.to_s
 	
 end
 
@@ -19,6 +26,14 @@ end
 #should display: 15120
 #because 5*6*7*8*9 = 15120
 get '/product' do
+	$s = params["start"].to_i
+	$e = params["end"].to_i
+	$sum = 1
+	while ( $s <= $e)
+		$sum *= $s
+		$s+=1
+	end
+	return $sum.to_s
 
 end
 
@@ -30,7 +45,15 @@ end
 get '/login' do
 	real_username = "root"
 	real_password = "secret"
+
+	user = params["username"]
+	pass = params["password"]
+
+
 	#fill in the rest
+		if user == real_username  and pass == real_password
+			return "Successfully Authenticated"
+		else
+			return "Error - Unauthorized"
+		end
 end
-
-
